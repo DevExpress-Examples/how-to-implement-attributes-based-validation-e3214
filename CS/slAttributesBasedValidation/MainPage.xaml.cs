fd857e1f89a4bf2f06b7c00663f7a310f57432dc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/slAttributesBasedValidation/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace slAttributesBasedValidation {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = Stuff.GetStuff();
+        }
+    }
+}
---
+++ VB/slAttributesBasedValidation/MainPage.xaml.vb
@@ -0,0 +1,12 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+
+Namespace slAttributesBasedValidation
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = Stuff.GetStuff()
+		End Sub
+	End Class
+End Namespace
